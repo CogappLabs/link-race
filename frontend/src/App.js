bd38eaa49ef16ba367086706a18d2f82a304f688
NitinL <nitinl@cogapp.com>
2021-11-22 17:56:53 +0000

    Add state to track progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import React, { useState } from "react";
 import "./App.css";
 import Path from "./components/path";
 import path from "./data/single-path.json";
 
 function App() {
+  const [progress, setProgress] = useState(1);
+
   return (
     <div>
-      <Path path={path} />
+      <Path path={path.slice(0, progress)} />
     </div>
   );
 }
